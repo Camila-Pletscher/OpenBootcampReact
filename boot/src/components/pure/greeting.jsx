21c41f71c5b9d2dbb9a5fb9ff8f10eddd069f901
CamiPle <camipletscher@gmail.com>
2023-05-08 12:42:53 -0300

    organization components, task components, class

---
+++ boot/src/components/pure/greeting.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+class Greeting extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hola</h1>
+            </div>
+        )
+    }
+}
+
+Greeting.propTypes = {
+
+}
+
+export default Greeting;
